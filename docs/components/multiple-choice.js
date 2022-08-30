0fb262544c401ac82dc1bc80259eef7d3d2d54a4
pammurray10 <pammcinnis10@gmail.com>
2022-08-30 15:28:56 -0400

    multiple choice component working

--- docs/components/multiple-choice.js
+++ docs/components/multiple-choice.js
@@ -1,18 +1,72 @@
 import React from "react";
 import { useState } from "react";
-import MultipleChoiceQuestion from "./multiple-choice-question";
 import { problem1, problem2 } from "./problems.js";
 
 export default function MultipleChoice() {
+  const [answers, setAnswers] = useState(problem1.answers);
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let selectedAnswer = answers.find((answer, index) => {
+      return answer.selected === true;
+    });
+    if (selectedAnswer.correct === true) {
+      console.log("correct");
+      setResponse("Correct!");
+    } else {
+      console.log("try again");
+      // turn the label element red for incorrect submission
+      setResponse("Try again.");
+    }
+  };
+
+  const handleClick = (index) => {
+    let newArr = [...answers];
+    answers.map((selectedAnswer, selectedIndex) => {
+      if (selectedIndex === index) {
+        newArr[index].selected = true;
+      } else {
+        newArr[selectedIndex].selected = false;
+      }
+    });
+    setAnswers(newArr);
+    console.log(answers);
+  };
+
+  const showAnswers = answers.map((answer, index) => {
+    return (
+      <div key={index}>
+        <input
+          type="radio"
+          id={index}
+          name="option"
+          value={answer.text}
+          onClick={() => {
+            handleClick(index);
+          }}
+        />
+        <label htmlFor={index}>{answer.text}</label>
+      </div>
+    );
+  });
+
   return (
     <div>
-      <MultipleChoiceQuestion problem={problem1} />
-
-      <p style={{ marginTop: "15px" }}>Correct!</p>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>{problem1.question}</legend>
+          {showAnswers}
+          <input type="submit" />
+          <p>{response}</p>
+        </fieldset>
+      </form>
     </div>
   );
 }
 
-// entire line is clickable
-// if correct option is clicked, entire line turns green and 'correct' appears at bottom of form
-// if incorrect option is clicked, entire line turns red and "try again" appears at bottom of form
+// if the correct answer is selected on submit, set the label background colour to green
+
+// Answers
+// - when you submit the form, there's a value: option="Sweet man"; if you don't include a value, it would say option=on
+// - use appearance: none on the radio to omit the style and add your own
--- docs/components/problems.js
+++ docs/components/problems.js
@@ -1,19 +1,19 @@
 export const problem1 = {
   question: "Who is Matthew?",
   answers: [
-    { answer: "Sweet man", correct: true },
-    { answer: "Alligator", correct: false },
-    { answer: "Tropical tree", correct: false },
-    { answer: "Shoemaker", correct: false },
+    { text: "Sweet man", correct: true, selected: false },
+    { text: "Alligator", correct: false, selected: false },
+    { text: "Tropical tree", correct: false, selected: false },
+    { text: "Shoemaker", correct: false, selected: false },
   ],
 };
 
 export const problem2 = {
   question: "Who is Matthew?",
   answers: [
-    { answer: "Sweet man", correct: true },
-    { answer: "Alligator", correct: false },
-    { answer: "Tropical tree", correct: false },
-    { answer: "Shoemaker", correct: false },
+    { text: "Sweet man", correct: true, selected: false },
+    { text: "Alligator", correct: false, selected: false },
+    { text: "Tropical tree", correct: false, selected: false },
+    { text: "Shoemaker", correct: false, selected: false },
   ],
 };
